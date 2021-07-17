a4c4367cd708877eb3deac36b42d5f842cdc5d50
thesuhas <suhastvs@gmail.com>
2021-07-17 23:39:29 +0530

    first commit

---
+++ build_images.sh
@@ -0,0 +1,6 @@
+cd ping
+sudo docker build -t ping .
+cd ../sql-injection
+sudo docker build -t sql-inject .
+cd ../nginx-server
+sudo docker build -t frontend-ui .
---
+++ reset-db.sh
@@ -0,0 +1,2 @@
+mysql -u root -p123456 -h 0.0.0.0 -e "CREATE DATABASE dvwa;"
+mysql -u root -p123456 dvwa -h 0.0.0.0 < data-dump.sql
