9678e5d9fb5cd8470ab6ac700fa9a7ca74b08dc1
Suhas Thalanki <54014218+thesuhas@users.noreply.github.com>
2021-07-18 16:52:02 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://localhost:8082/api/sqlquery"
-
-data = {"user_id": 1}
-
-res = requests.post(url=url, json = data, headers = {'Content-type': 'application/json'})
-print(res.text, res.status_code)
